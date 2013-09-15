e75d5a6fc8801eb8bce0e92114c19a93d6d4a402
irisel gonzalez <irisel@localhost.localdomain>
2013-09-14 23:12:50 -0430

    First Commit

---
+++ irianas_server/__init__.py
@@ -0,0 +1,10 @@
+""":mod:`irianas_server` -- Is receiving the request sent by the client
+"""
+
+from irianas_server import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
